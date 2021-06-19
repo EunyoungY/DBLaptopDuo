9c1d54cd2234d732646129f15a8d32896e850314
EunyoungY <21700432@handong.edu>
2021-06-20 00:47:58 +0900

    ranking

--- lib/major_ranking_list.dart
+++ lib/major_ranking_list.dart
@@ -19,8 +19,7 @@ class _MajorRankingPageState extends State<MajorRankingPage> {
   //  int _counter = 0;
   var db =  Mysql();
   var mail;
-
-
+  var laptop_info = [];
 
   // var results= await conn.query(
   // 'select name, email, age from users where id = ?', [result.insertId]);
@@ -31,6 +30,21 @@ class _MajorRankingPageState extends State<MajorRankingPage> {
 
   @override
   Widget build(BuildContext context) {
+    db.getConnection().then((conn) {
+
+      // String sql = ('select model_name from dbfinal.major_laptop_max where major=${widget.major}');
+      String sql = ('select * from dbfinal.major_laptop_max');
+      // String sql = ('select model_num from dbfinal.cart where user_num = 1');
+      conn.query(sql).then((results) {
+        for(var row in results){
+          setState(() {
+            laptop_info.add(row[0]);
+          });
+
+        }
+      });
+      conn.close();
+    });
 
     // db.getConnection().then((conn) {
     //   String sql = 'select C2 from laptopDUO.laptopDUO_model_name';
@@ -45,6 +59,7 @@ class _MajorRankingPageState extends State<MajorRankingPage> {
     // });
 
     return Scaffold(
+
       backgroundColor: Colors.white,
       appBar: DuoAppBar("전공별랭킹"),
       body: Padding(
@@ -53,24 +68,25 @@ class _MajorRankingPageState extends State<MajorRankingPage> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
-              color: const Color(0xff421F90),
-              height: 110,
-              width: MediaQuery.of(context).size.width,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  RichText(
-                    text: TextSpan(
-                      style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
-                      children: <TextSpan>[
-                        TextSpan(text: widget.major, style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),),
-                        TextSpan(text: '학생들이'),
-                      ],
+                color: const Color(0xff421F90),
+                height: 110,
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                        style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+                        children: <TextSpan>[
+                          TextSpan(text: widget.major, style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),),
+                          TextSpan(text: '학생들이'),
+                        ],
+                      ),
                     ),
-                  ),
-                  Text('가장 많이 사용하는 노트북', style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold))
-                ],
-              )
+
+                    Text('가장 많이 사용하는 노트북', style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold))
+                  ],
+                )
             ),
 
             SizedBox(height: 15),
@@ -117,7 +133,12 @@ class _MajorRankingPageState extends State<MajorRankingPage> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Center(child: Text('${mail}', style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold))),
+
+
+                  Container(width: 100,child: Text(laptop_info.isEmpty? ' ': laptop_info[0])),
+                  // Container(width: 100,child: Text(laptop_info.isEmpty? ' ': laptop_info[0], overflow: TextOverflow.ellipsis,)),
+
+                  // Center(child: Text('${mail}', style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold))),
 
                   // Text('${mail}', style: TextStyle(color: const Color(0xff2C2C2C), fontSize: 14, fontWeight: FontWeight.bold)),
 
